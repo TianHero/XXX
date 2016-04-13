433b847774953e7ec2869fe581a0ae9a1d7cf7d3
zhaotian <1246127205@qq.com>
2016-04-13 22:12:36 +0800

    ZT:ZT creaate a project named ZT

---
+++ ZT/ZT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZT
+//
+//  Created by scjy on 16/4/13.
+//  Copyright © 2016年 zhaotian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
